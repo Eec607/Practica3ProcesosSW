409be753d01d144917aed8ef2fd76b8b0bb4d818
Educala2000 <educala2000@gmail.com>
2021-11-22 16:42:33 +0100

    Añadido el fichero persistence.xml

--- Practica3Procesos/Practica3ProcesosCommon/src/main/java/es/unican/ps/Practica3Procesos/ClasesDominio/Supermercado.java
+++ Practica3Procesos/Practica3ProcesosCommon/src/main/java/es/unican/ps/Practica3Procesos/ClasesDominio/Supermercado.java
@@ -5,6 +5,11 @@ public class Supermercado {
 	private LocalTime horaApertura;
 	private LocalTime horaCierre;
 	
+	/**
+	 * Constructor de la clase Supermercado
+	 * @param horaApertura la hora de apertura del supermercado
+	 * @param horaCierre la hora de cierre del supermercado
+	 */
 	public Supermercado(LocalTime horaApertura, LocalTime horaCierre) {
 		this.horaApertura = horaApertura;
 		this.horaCierre = horaCierre;
